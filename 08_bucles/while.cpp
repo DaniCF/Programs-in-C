8c2ab5c37f129a97ea96c60be33f30cad2f051d2
DaniCF <dancf3@yahoo.es>
2016-01-11 18:28:41 +0100

    Update while.cpp

--- 08_bucles/while.cpp
+++ 08_bucles/while.cpp
@@ -1,20 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdio.h> //Librerias de Linux
+#include <stdlib.h> //Librerias de Linux
+#include <time.h> //Librerias de Tiempo
+
+//Genera un numero del 1 al 10 que hay que tratar de adivinar.
 
 int main(int argc, const char **argv){
-    int numero, pensado;
+    int numero, pensado; //Variables
 
-    printf("Adivina el numero entre 1 y 10.\n");
+    printf("Adivina el numero entre 1 y 10.\n"); //El programa imprime el primer enunciado.
 
     srand(time(NULL));
-    pensado = rand() % 10 + 1;
+    pensado = rand() % 10 + 1; //Estas dos lineas generan el numero aleatorio.
     do {
         printf("Numero: ");
         scanf(" %i", &numero);
-    } while( numero != pensado );
+    } while( numero != pensado ); //Ciclo que se repite hasta acertar el numero
 
-    printf("Mu bien.\n");
+    printf("Mu bien.\n");  //Mensaje una vez acertado el numero.
 
     return EXIT_SUCCESS;
 }
